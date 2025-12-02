6cae76066e14344c3726c62053f34ace25d8342f
JeongDay <cidd1332@gmail.com>
2025-12-02 16:00:56 +0900

    2025.12.02 화요일 - 오후시간 3
    모험가 키보드 조작

--- Assets/_Platformer/02. Scripts/AdventurerMovement.cs
+++ Assets/_Platformer/02. Scripts/AdventurerMovement.cs
@@ -1,24 +1,30 @@
+using System;
 using UnityEngine;
 
 public class AdventurerMovement : MonoBehaviour
 {
     private Animator anim;
     private Rigidbody2D rb;
+    private CapsuleCollider2D coll;
 
     private float h, v;
 
     [SerializeField] private float moveSpeed = 3f;
+    [SerializeField] private float jumpPower = 7f;
 
     void Start()
     {
         anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<CapsuleCollider2D>();
     }
 
     void Update()
     {
         h = Input.GetAxisRaw("Horizontal");
         v = Input.GetAxisRaw("Vertical");
+
+        Jump();
     }
 
     void FixedUpdate()
@@ -27,11 +33,38 @@ public class AdventurerMovement : MonoBehaviour
         Move();
     }
 
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            anim.SetBool("IsGround", true);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            anim.SetBool("IsGround", false);
+        }
+    }
+
     private void Move()
     {
         rb.linearVelocityX = h * moveSpeed;
     }
 
+    private void Jump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            anim.SetBool("IsGround", false);
+
+            anim.SetTrigger("Jump");
+            rb.AddForceY(jumpPower, ForceMode2D.Impulse);
+        }
+    }
+    
     private void SetAnimation()
     {
         if (h != 0)
@@ -48,6 +81,20 @@ public class AdventurerMovement : MonoBehaviour
         //     float scaleX = h > 0 ? 1 : -1;
         //     transform.localScale = new Vector3(scaleX, 1, 1);
         // }
+
+        if (v < 0) // Crouch 상태
+        {
+            coll.offset = new Vector2(coll.offset.x, 0.55f);
+            coll.size = new Vector2(coll.size.x, 1.1f);
+            moveSpeed = 1.5f;
+        }
+        else if (v >= 0) // Idle 상태
+        {
+            coll.offset = new Vector2(coll.offset.x, 0.8f);
+            coll.size = new Vector2(coll.size.x, 1.6f);
+            moveSpeed = 3f;
+        }
+        
         
         anim.SetFloat("AxisX", h);
         anim.SetFloat("AxisY", v);
